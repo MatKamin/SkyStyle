9fab22438f62b7907ad1534eeb9646f7cc0fc695
clemjberg <clemens.jae@gmail.com>
2024-06-19 16:07:14 +0200

    fetch
    fetch in frontend added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -21,13 +21,15 @@ myApp.use(Quasar, {
 
 myApp.mount('#app')
 
-/**
+
 fetch('/api/weather')
 .then(res => {
   if (!res.ok) {
     throw new Error('Network Response was not ok');
   }
+  console.log("1");
   console.log(res);
+  console.log("2");
   return res.json();
 })
 .then(data => {
@@ -36,4 +38,19 @@ fetch('/api/weather')
 .catch(err => {
   console.error('Error fetching weather data:', err);
 })
-*/
+
+
+fetch('/passwd')
+.then(res => {
+  if (!res.ok) {
+    throw new Error('Network Response was not ok!');
+  }
+  console.log(res);
+  return res.json();
+})
+.then(d => {
+  console.log(d);
+})
+.catch(e => {
+  console.error('Error fetching weather data:', e);
+});
